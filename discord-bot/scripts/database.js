d2369de953f5ea7b452a533fdb05e2e598889d54
LAPTOP-4ONCPGNL\Mtran <mtran0011@outlook.com>
2021-07-19 22:17:43 +1000

    Fixed the path bug when using absolute paths.

--- discord-bot/scripts/database.js
+++ discord-bot/scripts/database.js
@@ -2,9 +2,12 @@ const path = require('path');
 const { AsyncNedb } = require('nedb-async')
 
 const Datastore = AsyncNedb;
-const dbPath = process.env.DB_PATH ?? '/../data/groups.db'
+let dbPath = process.env.DB_PATH ?? '/data/groups.db'
+
+if (dbPath.startsWith('.'))
+  dbPath = path.join(`${__dirname}/..`, dbPath) // Make it relative to the root project directory
 
 db = {};
-db.groups = new Datastore({ filename: path.join(__dirname, dbPath), autoload: true });
+db.groups = new Datastore({ filename: dbPath, autoload: true });
 
 module.exports = db;
